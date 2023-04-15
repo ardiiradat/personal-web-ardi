9821a06cb89dffe50035b7b229d1ac7bc78dfc2c
ardi5 <ardi@analisa.io>
2023-04-15 14:36:28 +0700

    update portofolio page with slider

--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -37,13 +37,6 @@ const index = () => {
                 <PersonalInfo />
               </div>
               {/* End personal info */}
-
-              <div className="col-12 mt-1">
-                <a className="button" href={cv} download>
-                  <span className="button-text">Download CV</span>
-                  <span className="button-icon fa fa-download"></span>
-                </a>
-              </div>
               {/* End download button */}
             </div>
           </div>
---
+++ src/components/portfolio/slider.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import Slider from "react-slick";
+
+function slider() {
+
+  var settings = {
+    infinite: true,
+    dots: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    fade: true,
+    arrows: true
+  };
+  return (
+    <Slider {...settings}>
+      <div>
+        <h1 className="text-center">Analisa.io</h1>
+        <a target="_blank" style={{textDecoration: "none"}} href="https://analisa.io">
+        <img src="/assets/img/portfolio/mockup-analisa.png" alt="" />
+        </a>
+      </div>
+      <div>
+        <h1 className="text-center">Hawksight.co</h1>
+        <a target="_blank" style={{textDecoration: "none"}} href="https://hawksight.co">
+          <img src="/assets/img/portfolio/mockup-hawksight.png" alt="" />
+        </a>
+      </div>
+      <div>
+        <h1 className="text-center">MandiriKartuKredit.com</h1>
+        <a target="_blank" style={{textDecoration: "none"}} href="https://mandirikartukredit.com">
+          <img src="/assets/img/portfolio/mockup-mandiri-kartukredit.png" alt="" />
+        </a>
+      </div>
+    </Slider>
+  )
+}
+
+export default slider
